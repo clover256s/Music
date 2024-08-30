765a96f7634e62921ea78d59d4c513e586ea3995
xuwenxiang <xuwenxiang256@outlook.com>
2024-08-30 19:59:15 +0800

    bug w c n m d a

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use mp3::get_music_cover;
-mod mp3;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
-    get_music_cover("きのこ帝国 - School／Fiction.mp3".to_string());
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
